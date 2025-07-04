61f285b24a7277324f80e976ad0b1909784fd57c
Hawin <hawin.caraballo@softwareone.com>
2025-07-04 13:57:04 -0500

    change in file startup and remove dockerfile

--- startup.sh
+++ startup.sh
@@ -1,1 +1,7 @@
-fastapi run src\extracted-pdf-api\main.py
+#!/bin/bash
+
+# Activar el entorno virtual si es necesario
+# source .venv/bin/activate
+
+# Ejecutar la aplicación con Uvicorn
+exec uvicorn src.extracted_pdf_api.main:app 
